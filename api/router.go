0a0ec63f83146cf2320c131d8340af3aa817f4e1
developer <developer@gmail.com>
2024-05-03 21:19:29 +0530

    feat(router): added router for APIs

---
+++ api/router.go
@@ -0,0 +1,23 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	v1 := router.Group("/api/v1")
+
+	users := v1.Group("/users")
+	{
+		users.POST("/login", LoginAPI)
+		users.GET("/home", UserHomeAPI)
+	}
+
+	onboarding := v1.Group("/books")
+	{
+		onboarding.POST("/addBook", AddBookAPI)
+		onboarding.DELETE("/deleteBook", DeleteBookAPI)
+	}
+
+	return router
+}
